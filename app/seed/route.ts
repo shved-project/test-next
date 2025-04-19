f3b428655c4906a7d8c979d56ce0955b92be1663
Швецов Александр <shvitc@gmail.com>
2025-04-19 23:29:36 +0300

    Учим Next.js

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -4,7 +4,7 @@ import {invoices, customers, revenue, users} from "../lib/placeholder-data";
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: "require"});
 
-async function seedUsers() {
+async function seedUsers(sql: postgres.Sql) {
 	await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 	await sql`
     CREATE TABLE IF NOT EXISTS users (
@@ -29,7 +29,7 @@ async function seedUsers() {
 	return insertedUsers;
 }
 
-async function seedInvoices() {
+async function seedInvoices(sql: postgres.Sql) {
 	await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
 	await sql`
@@ -55,7 +55,7 @@ async function seedInvoices() {
 	return insertedInvoices;
 }
 
-async function seedCustomers() {
+async function seedCustomers(sql: postgres.Sql) {
 	await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
 	await sql`
@@ -80,7 +80,7 @@ async function seedCustomers() {
 	return insertedCustomers;
 }
 
-async function seedRevenue() {
+async function seedRevenue(sql: postgres.Sql) {
 	await sql`
     CREATE TABLE IF NOT EXISTS revenue (
       month VARCHAR(4) NOT NULL UNIQUE,
@@ -103,7 +103,12 @@ async function seedRevenue() {
 
 export async function GET() {
 	try {
-		const result = await sql.begin((sql) => [seedUsers(), seedCustomers(), seedInvoices(), seedRevenue()]);
+		const result = await sql.begin(async (tx) => [
+			seedUsers(tx),
+			seedCustomers(tx),
+			seedInvoices(tx),
+			seedRevenue(tx),
+		]);
 
 		return Response.json({message: "Database seeded successfully"});
 	} catch (error) {
